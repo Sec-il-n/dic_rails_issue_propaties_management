c53386fcac01c1af6edb46db791bc9d192a04f0f
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-21 14:24:57 +0900

    物件情報と最寄り駅登録機能

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,12 +2,14 @@ class PropertiesController < ApplicationController
   before_action :set_property, only:[:show, :edit, :update]
   def new
     @property = Property.new
-    @property.stations.build
+    # @property.stations.build
+    # @form = Form::StationCollection.new
     #フォームを指定回数表示させたい場合。
     # 2.times(@property.stations.build)
+    2.times.map{@property.stations.build}
   end
   def show
-
+    
   end
   def index
     @properties = Property.all
@@ -16,10 +18,14 @@ class PropertiesController < ApplicationController
   end
   def create
     @property = Property.new(property_params)
-    unless @property.save then
-      render :new
+    # @form = Form::StaionCollection.new(station_collection_params)
+    if @property.save
+      redirect_to  properties_path, notice: %(物件情報を登録しました。)
+    # elsif @form.save
+    #   redirect_to  properties_path, notice:  %(最寄り駅を登録しました。)
     else
-      redirect_to  properties_path, notice: '物件情報を登録しました。'
+      flash.now[:danger]=%(登録に失敗しました。)
+      render :new
     end
   end
   def update
@@ -28,10 +34,9 @@ class PropertiesController < ApplicationController
   end
   private
   def set_property
-    @property = Property.find_by(params[:id])
+    @property = Property.find_by(id: params[:id])
   end
   def property_params
-    params.require(:property).permit(:name, :price, :address, :age, :note, stations_attributes:[:line, :station_name, :minute, :property_id])
-    # params.require(:property).permit(:name, :price, :address, :age, :note)
+    params.require(:property).permit(:name, :price, :address, :age, :note, {stations_attributes:[:line, :station_name, :minute, :property_id]})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'properties#new'
   resources :properties
-  resources :stations, only: [:new, :index, :create, :edit, :update]
+  resources :stations, only: [:new, :create, :edit, :update]
+  # resources :stations, only: [:new, :index, :create, :edit, :update]
 end
